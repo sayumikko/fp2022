8e1742e1437267daa9d8a295b49954cbfcf6ee80
sayumikko <kuksant2708@mail.ru>
2023-02-24 22:53:28 +0300

    Formatted forgotten files

--- bash/demos/demoParser.ml
+++ bash/demos/demoParser.ml
@@ -5,5 +5,6 @@ let parse_print s =
   match parse_string ~consume:Consume.All (ast_parser ()) s with
   | Error e -> print_endline ("ERROR" ^ e)
   | Ok b -> print_endline (Bash_lib.Ast.show_ast b)
+;;
 
 let () = parse_print (Stdio.In_channel.input_all Caml.stdin)
